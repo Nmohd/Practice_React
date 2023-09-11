c6e58eba60b44b0edfa4a5c0af7f48200d508b39
Nmohd <najeebu50@gmail.com>
2023-09-11 08:32:03 +0530

    done

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import Logo from "./Logo";
-import Form from "./Form";
-import Stats from "./Stats";
-import PackingList from "./PackingList";
+import Logo from "./Components/Logo";
+import Form from "./Components/Form";
+import Stats from "./Components/Stats";
+import PackingList from "./Components/PackingList";
 
 // const initialItems = [
 //   { id: 1, description: "Passports", quantity: 2, packed: false },
