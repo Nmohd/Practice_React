5425604540e3feadd7ddb581614330538861d387
Nmohd <najeebu50@gmail.com>
2023-09-26 08:40:48 +0530

    fetching data

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 import StarRating from "./StarRating";
 import { useState } from "react";
 
@@ -18,13 +18,13 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+     <App /> 
     {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     /> */}
 
-    <Test />
+    {/* <Test /> */}
   </React.StrictMode>
 );
 
